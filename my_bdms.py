cedc8a1b23200f87021508ba4e386b4b4176a0f5
WSDeWitt <wsdewitt@gmail.com>
2024-01-25 22:20:19 -0800

    format/lint scripts and notebooks

--- my_bdms.py
+++ my_bdms.py
@@ -1,22 +1,25 @@
 from typing import Protocol, Hashable, Callable, Optional, Union, Tuple
 import numpy as np
-
 import ete3
 
-import bdms
 from bdms import poisson, mutators
 import modulators
 
+
 class ListOfHashables(Protocol):
     r"""
-        Definition of type which includes any type which supports indexing with an integer
-        and a length operation.
+    Definition of type which includes any type which supports indexing with an
+    integer and a length operation.
     """
-    def __getitem__(self, index: int) -> Hashable: ...
-    def __len__(self) -> int: ...
+
+    def __getitem__(self, index: int) -> Hashable:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
 
 class DiscreteProcess(poisson.Process):
-
     def __init__(
         self,
         state_space: ListOfHashables,
@@ -25,66 +28,67 @@ class DiscreteProcess(poisson.Process):
         super().__init__()
         self.state_space = state_space
         self.rates = rates
-        self.rate_dict = {state:rate for state, rate in zip(state_space,rates)}
-    
+        self.rate_dict = {state: rate for state, rate in zip(state_space, rates)}
+
     def λ(self, x: Hashable, t: float) -> float:
         return self.rate_dict[x]
-    
+
     def Λ(self, x: Hashable, t: float, Δt: float) -> float:
-        return self.rate_dict[x]*Δt
-    
+        return self.rate_dict[x] * Δt
+
     def Λ_inv(self, x: Hashable, t: float, τ: float) -> float:
-        return τ/self.rate_dict[x] if self.rate_dict[x] > 0 else np.inf  
-    
+        return τ / self.rate_dict[x] if self.rate_dict[x] > 0 else np.inf
+
     def _param_dict(self):
         return None
-    
-class CustomProcess(poisson.Process):
 
+
+class CustomProcess(poisson.Process):
     def __init__(
         self,
-        λ: Callable[[Hashable,float],float],
-        Λ: Callable[[Hashable,float,float],float],
-        Λ_inv: Callable[[Hashable,float,float],float],
+        λ: Callable[[Hashable, float], float],
+        Λ: Callable[[Hashable, float, float], float],
+        Λ_inv: Callable[[Hashable, float, float], float],
     ):
         super().__init__()
         self._λ = λ
         self._Λ = Λ
         self._Λ_inv = Λ_inv
-    
+
     def λ(self, x: Hashable, t: float) -> float:
-        return self._λ(x,t)
-    
+        return self._λ(x, t)
+
     def Λ(self, x: Hashable, t: float, Δt: float) -> float:
-        return self._Λ(x,t,Δt)
-    
+        return self._Λ(x, t, Δt)
+
     def Λ_inv(self, x: Hashable, t: float, τ: float) -> float:
-        return self._Λ_inv(x,t,τ)
-    
+        return self._Λ_inv(x, t, τ)
+
     def _param_dict(self):
         return None
-    
-class CustomMutator(mutators.Mutator):
 
+
+class CustomMutator(mutators.Mutator):
     def __init__(
         self,
         modulator: modulators.FEModulator,
     ):
         super().__init__()
         self.modulator = modulator
-    
+
     def mutate(
         self,
         node: ete3.TreeNode,
         seed: Optional[Union[int, np.random.Generator]] = None,
     ) -> None:
-        
         if isinstance(seed, np.random.Generator):
             rng = seed
         else:
             rng = np.random.default_rng(seed)
-        
-        new_phenotype = rng.choice(self.modulator.state_space,size=1,p=self.modulator.mutation_probs(node))[0]
+
+        new_phenotype = rng.choice(
+            self.modulator.state_space, size=1, p=self.modulator.mutation_probs(node)
+        )[0]
         setattr(node, self.modulator.attr, new_phenotype)
 
     def logprob(self, node: ete3.TreeNode) -> float:
--- utils.py
+++ utils.py
@@ -1,26 +1,30 @@
 import numpy as np
 from collections import Counter
 
-import ete3
 
-def clamp(i,left,right):
-    return min(max(i,left),right)
+def clamp(i, left, right):
+    return min(max(i, left), right)
+
 
 def grid_interp(x: float, fp: np.ndarray, x_min: float, dx: float):
     r"""
-        For function :math:`f` defined by f(x_min + i*dx) = fp[i], compute
-        f(x) by linear interpolation.
+    For function :math:`f` defined by f(x_min + i*dx) = fp[i], compute
+    f(x) by linear interpolation.
     """
-    
-    x_max = x_min+(len(fp)-1)*dx
-    x_clamp = clamp(x,x_min,x_max)
-    x_left_idx = clamp(int(np.floor((x_clamp - x_min)/dx)),0,len(fp)-1)
-    x_right_idx = clamp(int(np.ceil((x_clamp - x_min)/dx)),0,len(fp)-1)
 
-    return np.interp(x_clamp,[x_min + dx*x_left_idx,x_min + dx*x_right_idx],fp[[x_left_idx,x_right_idx]])
+    x_max = x_min + (len(fp) - 1) * dx
+    x_clamp = clamp(x, x_min, x_max)
+    x_left_idx = clamp(int(np.floor((x_clamp - x_min) / dx)), 0, len(fp) - 1)
+    x_right_idx = clamp(int(np.ceil((x_clamp - x_min) / dx)), 0, len(fp) - 1)
+
+    return np.interp(
+        x_clamp,
+        [x_min + dx * x_left_idx, x_min + dx * x_right_idx],
+        fp[[x_left_idx, x_right_idx]],
+    )
+
 
 def tree_size(tree):
-
     num_events = 0
 
     for _ in tree.traverse("preorder"):
@@ -28,71 +32,100 @@ def tree_size(tree):
 
     return num_events
 
-def num_subtrees_by_size(tree):
 
+def num_subtrees_by_size(tree):
     size_by_node = {}
 
     for node in tree.traverse("postorder"):
         if node.is_leaf():
             size_by_node[node.name] = 1
         else:
-            size_by_node[node.name] = sum([size_by_node[child.name] for child in node.children])
+            size_by_node[node.name] = sum(
+                [size_by_node[child.name] for child in node.children]
+            )
 
     subtree_counts = Counter(size_by_node.values())
     return subtree_counts
 
-def total_branch_length(tree):
 
-    return {"total":sum([node.dist for node in tree.traverse() if not node.is_root()])}
+def total_branch_length(tree):
+    return {"total": sum([node.dist for node in tree.traverse() if not node.is_root()])}
+
 
 def num_lineages_by_phenotype_and_time(tree, t):
+    return Counter(
+        [
+            node.state
+            for node in tree.iter_descendants()
+            if node.t >= t and node.up.t <= t
+        ]
+    )
 
-    return Counter([node.state for node in tree.iter_descendants() if node.t >= t and node.up.t <= t])
 
 def num_nodes_by_phenotype(tree):
-
     return Counter([node.state for node in tree.traverse()])
 
-def num_leaves_by_phenotype(tree):
 
+def num_leaves_by_phenotype(tree):
     return Counter([node.state for node in tree.traverse() if node.is_leaf()])
 
-def branch_length_by_phenotype(tree):
 
+def branch_length_by_phenotype(tree):
     branch_lengths = {}
-    
+
     for node in tree.iter_descendants():
         if node.up.state not in branch_lengths:
             branch_lengths[node.up.state] = 0.0
         branch_lengths[node.up.state] += node.dist
-    
+
     return branch_lengths
 
-def clade_sizes_by_phenotype(tree):
 
+def clade_sizes_by_phenotype(tree):
     clade_sizes = []
 
     subclade_size_by_node = {}
     for node in tree.traverse("postorder"):
+        subclade_size_by_node[node.name] = 1 + sum(
+            [
+                subclade_size_by_node[child.name]
+                for child in node.children
+                if child.state == node.state
+            ]
+        )
+        if (
+            node.up is None or node.up.state != node.state
+        ):  # in this case, node is the root of a subclade
+            clade_sizes.append(
+                {"phenotype": node.state, "value": subclade_size_by_node[node.name]}
+            )
 
-        subclade_size_by_node[node.name] = 1 + sum([subclade_size_by_node[child.name] for child in node.children if child.state == node.state])
-        if node.up is None or node.up.state != node.state: # in this case, node is the root of a subclade
-            clade_sizes.append({"phenotype":node.state,"value":subclade_size_by_node[node.name]})
-    
     return clade_sizes
 
-def clade_lengths_by_phenotype(tree):
 
+def clade_lengths_by_phenotype(tree):
     clade_lengths = []
 
     subclade_length_by_node = {}
     for node in tree.traverse("postorder"):
+        subclade_length_by_node[node.name] = sum(
+            [child.dist for child in node.children]
+        ) + sum(
+            [
+                subclade_length_by_node[child.name]
+                for child in node.children
+                if child.state == node.state
+            ]
+        )  # branch associate with parent phenotype
+        if (
+            node.up is None or node.up.state != node.state
+        ):  # in this case, node is the root of a subclade
+            clade_lengths.append(
+                {"phenotype": node.state, "value": subclade_length_by_node[node.name]}
+            )
 
-        subclade_length_by_node[node.name] = sum([child.dist for child in node.children]) + sum([subclade_length_by_node[child.name] for child in node.children if child.state == node.state]) # branch associate with parent phenotype
-        if node.up is None or node.up.state != node.state: # in this case, node is the root of a subclade
-            clade_lengths.append({"phenotype":node.state,"value":subclade_length_by_node[node.name]})
-    
     return clade_lengths
+
 
 def forward_moment_equation(
     state_space,
